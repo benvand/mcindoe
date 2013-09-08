24fccb0e2c4b286c36ae76a888ec85b4e2aa9156
benvand <benjamin_vandersteen@hotmail.com>
2013-09-09 09:56:34 +1000

    400 500

--- mcindoe/views.py
+++ mcindoe/views.py
@@ -10,3 +10,9 @@ def comingSoon(request):
     Site = get_current_site(request)
     context = {'global_site':Site,}
     return render(request, 'comingsoon.html', context)
+
+def fiveHundred(request):
+    return render(request, '500.html')
+
+def fourHundred(request):
+    return render(request, '400.html')
