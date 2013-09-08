9f4819a684395eda7e4d0e5819f036defc3c1656
benvand <benjamin_vandersteen@hotmail.com>
2013-09-08 23:50:00 +0000

    working dev

--- mcindoe/urls.py
+++ mcindoe/urls.py
@@ -20,7 +20,7 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     url(r'^maddyonly/', include(admin.site.urls)),
-    url(r'^contact/', include(contacturls)),
+#    url(r'^contact/', include(contacturls)),
 )
 
 #Stuff to serve static admin content
