0d03e9703f9b398de65e342b1be8a8b7e5758ebd
benvand <benjamin_vandersteen@hotmail.com>
2013-11-19 13:17:26 +0000

    debugging

--- mcindoe/settings.py
+++ mcindoe/settings.py
@@ -11,6 +11,7 @@ DEV = DEBUG = TEMPLATE_DEBUG = True if os.uname()[1] in ['ben-virtualbox', 'ben-
 FASTPROD=False
 STAGEDPROD= True if not (DEV or FASTPROD) else False #Explicit switch to be False on final deployment
 PROD= True if not (STAGEDPROD or FASTPROD or DEV) else False
+print DEV, FASTPROD, STAGEDPROD, PROD
 
 ADMINS = (
     ('ben','benjaminfvandersteen@gmail.com'),
