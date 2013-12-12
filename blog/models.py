10cba203b1160159ed549239b375bfea41f05df0
benvand <benjamin_vandersteen@hotmail.com>
2013-12-12 12:49:19 +0000

    unicode all models, blog html, model change

--- blog/models.py
+++ blog/models.py
@@ -10,10 +10,13 @@ User = get_user_model()
 class Post(models.Model):
     title =             models.CharField(max_length=255)
     content =           models.TextField()
-    created =           models.TimeField(auto_now_add=True)
-    modified =          models.TimeField(auto_now=True)
+    created =           models.DateTimeField(auto_now_add=True)
+    modified =          models.DateTimeField(auto_now=True)
     userid =            models.ForeignKey(User, related_name='userblogpost')
     PostPicture = None
+
+    def __unicode__(self):
+        return self.title
 
     class Meta():
         ordering = ('created',)
@@ -27,6 +30,9 @@ class Comment(models.Model):
     modified =          models.TimeField(auto_now=True)
     userid =            models.ForeignKey(User, related_name='userblogcomment')
     postid =            models.ForeignKey('Post')
+
+    def __unicode__(self):
+        return self.userid + ': ' + self.content[:20]
 
     class Meta():
         ordering = ('created',)
--- blog/views.py
+++ blog/views.py
@@ -2,10 +2,11 @@
 # Create your views here.
 from django.shortcuts import render
 from django.contrib.sites.models import get_current_site
-from models import BlogOptions
+from models import BlogOptions, Post
+from django.contrib.auth.models import User
 
 def blog(request):
     site = get_current_site(request)
     blog = BlogOptions.objects.get(site=site)
-    context = {'global_site': site, "blog": blog}
+    context = {'global_site': site, "blog": blog, "posts": Post.objects.all }
     return render(request, 'blog/blog.html', context)
